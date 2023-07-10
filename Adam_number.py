7a7894350e1a3ac17a30bd6929a12df5f43c46e6
thandrangiashok <114074710+thandrangiashok@users.noreply.github.com>
2023-07-10 09:54:16 +0530

    n=int(input()) sq=n*n n=str(n) n=n[::-1] n=int(n) sq2=n*n sq2=str(sq2) sq2=sq2[::-1] sq2=int(sq2) print(sq==sq2)

--- Adam_number.py
+++ Adam_number.py
@@ -1,15 +1,10 @@
 n=int(input())
-s=n*n
-rev=0
-while n>0:
-    r=n%10
-    rev=rev*10+r
-    n=n//10
-k=rev*rev
-rev1=0
-while k>0:
-    r=k%10
-    rev1=rev1*10+r
-    k=k//10
-print(rev1==s)
-    
+sq=n*n
+n=str(n)
+n=n[::-1]
+n=int(n)
+sq2=n*n
+sq2=str(sq2)
+sq2=sq2[::-1]
+sq2=int(sq2)
+print(sq==sq2)
