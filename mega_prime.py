6e52ba4a8d608a8f266b07a9c32e1a961dc586ef
thandrangiashok <114074710+thandrangiashok@users.noreply.github.com>
2023-07-10 09:49:13 +0530

    def p(n):     pr=True     if n==1:         return False     for i in range(2,int(n**0.5)+1):         if n%i==0:             pr=False     return pr n=int(input()) if p(n)==True:     c=0     d=0     while n>0:         r=n%10         c+=1         if p(r)==True:             d+=1         n=n//10     if c==d:         print("Mega Prime")     else:         print("Not Mega Prime") else:     print("Not Mega Prime")

--- mega_prime.py
+++ mega_prime.py
@@ -1,24 +1,24 @@
-def prime(n):
-    is_prime=True
+def p(n):
+    pr=True
     if n==1:
         return False
     for i in range(2,int(n**0.5)+1):
         if n%i==0:
-            is_prime=False
-    return is_prime
+            pr=False
+    return pr
 n=int(input())
-if prime(n)==True:
+if p(n)==True:
     c=0
     d=0
     while n>0:
         r=n%10
         c+=1
-        if prime(r)==True:
+        if p(r)==True:
             d+=1
         n=n//10
     if c==d:
-        print('Mega Prime')
+        print("Mega Prime")
     else:
-        print('Not Mega Prime')
+        print("Not Mega Prime")
 else:
-    print('Not Mega Prime')
+    print("Not Mega Prime")
