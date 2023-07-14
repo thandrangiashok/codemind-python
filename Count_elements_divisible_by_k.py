fb3b950dea1bdcdab2a75f9fd362512fd75d2e7e
thandrangiashok <114074710+thandrangiashok@users.noreply.github.com>
2023-07-14 20:59:09 +0530

    def count_elements_divisible(arr, K):     count = 0
        # Check each element in the array
        for num in arr:
            if num % K == 0:
                count += 1
    
        return count
    
    # Read input
    N, K = map(int, input().split())
    arr = list(map(int, input().split()))
    
    # Get the count of elements divisible by K
    result = count_elements_divisible(arr, K)
    
    # Print the result
    print(result)

---
+++ Count_elements_divisible_by_k.py
@@ -0,0 +1,19 @@
+def count_elements_divisible(arr, K):
+    count = 0
+
+    # Check each element in the array
+    for num in arr:
+        if num % K == 0:
+            count += 1
+
+    return count
+
+# Read input
+N, K = map(int, input().split())
+arr = list(map(int, input().split()))
+
+# Get the count of elements divisible by K
+result = count_elements_divisible(arr, K)
+
+# Print the result
+print(result)
